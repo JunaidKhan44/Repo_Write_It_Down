0f13063c0fb3c05670c949ab3bb50db22a057f65
JunaidKhan44 <junaidmanimani555@gmail.com>
2021-05-30 09:59:57 +0500

    Updated code

--- app/src/main/java/com/theswiftvision/writeitdown/Adapters/ReminderAdapter.java
+++ app/src/main/java/com/theswiftvision/writeitdown/Adapters/ReminderAdapter.java
@@ -2,6 +2,8 @@ package com.theswiftvision.writeitdown.Adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,9 +18,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.theswiftvision.writeitdown.ModelClasses.AlarmDetails;
 import com.theswiftvision.writeitdown.R;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 
 public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.ViewHolder> {
     ArrayList<AlarmDetails> alarmDetailsList = new ArrayList<>();
@@ -26,7 +30,8 @@ public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.ViewHo
     Calendar calendar;
     private SimpleDateFormat dateFormat;
     private String date, tomorrow;
-    private String time = String.valueOf(System.currentTimeMillis()-1);
+    private String time = String.valueOf(System.currentTimeMillis() - 1);
+    String currentDate = null;
 
     public ReminderAdapter(ArrayList<AlarmDetails> alarmDetailsList, Context context) {
         this.alarmDetailsList = alarmDetailsList;
@@ -46,26 +51,73 @@ public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.ViewHo
         holder.alarmTitle.setText(alarmDetails.getmTitle());
         holder.alarmTime.setText(alarmDetails.getmTime());
 
+        //get date
         calendar = Calendar.getInstance();
         dateFormat = new SimpleDateFormat("dd-MM-yyyy");
         date = dateFormat.format(calendar.getTime());
 
-        if (alarmDetails.getmDate().equals(date)&&alarmDetails.getmTime().equals(time)) {
+
+        /**/
+        Date alarmDate = null;
+        Date currentDate = null;
+        try {
+
+            alarmDate = dateFormat.parse(alarmDetails.getmDate());
+            currentDate = dateFormat.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        /**/
+
+        /**/
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String getCurrentTime = sdf.format(c.getTime());  //current time
+        String getTestTime = alarmDetails.getmTime(); //old time
+
+//        if (getCurrentTime .compareTo(getTestTime) < 0 &&)
+//        {
+//            // Do your staff
+//            Log.d("Return","getTestTime less than getCurrentTime ");
+//
+//        }
+//        else
+//
+        if (getCurrentTime.compareTo(getTestTime) > 0 && currentDate.equals(alarmDate)) {
+            Log.d("Return", "getTestTime older than getCurrentTime ");
             holder.alarmStatus.setImageResource(R.drawable.alarm_done);
             holder.clock.setVisibility(View.INVISIBLE);
             holder.alarmDone.setVisibility(View.VISIBLE);
+            holder.cardView.setEnabled(false);
+            
+        }else{
+            holder.cardView.setEnabled(true);
         }
+        /**/
+
+
+//
+//        if (alarmDetails.getmDate().equals(date)&&alarmDetails.getmTime().equals(time)) {
+//            holder.alarmStatus.setImageResource(R.drawable.alarm_done);
+//            holder.clock.setVisibility(View.INVISIBLE);
+//            holder.alarmDone.setVisibility(View.VISIBLE);
+//            Log.d("tt","equals call");
+//        }
         holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(context, com.theswiftvision.writeitdown.Activities.AlarmDetails.class);
                 intent.putExtra("title", alarmDetails.getmTitle());
                 intent.putExtra("mNotes", alarmDetails.getmNotes());
                 intent.putExtra("time", alarmDetails.getmTime());
                 intent.putExtra("date", alarmDetails.getmDate());
                 context.startActivity(intent);
+
             }
         });
+
+        Log.d("tag", "today date" + alarmDetails.getmDate());
 
     }
 
